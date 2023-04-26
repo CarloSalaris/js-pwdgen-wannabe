6df8bd9409d5d011da4b379ca63976a62dc387fb
CarloSalaris <carloa.salaris@gmail.com>
2023-04-26 14:42:47 +0200

    Richiesta input Nome e Cognome

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
+// Variables
+
+// const userName, userSurname, userColour;
+
 // Chiedi all’utente il suo nome
+const userName = prompt("Inserisci il tuo nome");
+
+console.log(userName);
+
 
 // Chiedi il suo cognome
+
+const userSurname = prompt("Inserisci il tuo cognome");
+
+console.log(userSurname);
+
+
 
 // Chiedi il suo colore preferito
 
