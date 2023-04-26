4fcef3498f3e98c25619453a11f0c70edf0b2828
CarloSalaris <carloa.salaris@gmail.com>
2023-04-26 15:22:14 +0200

    JS Changed "let" to "const"

--- js/main.js
+++ js/main.js
@@ -1,31 +1,27 @@
-// Variables
-    let userName, userSurname, userColour, userPassword;
-    // const userName, userSurname, userColour;
-
 
 // Chiedi all’utente il suo nome
-userName = prompt("Inserisci il tuo nome");
+const userName = prompt("Inserisci il tuo nome");
 
 console.log(userName);
 
 
 // Chiedi il suo cognome
 
-userSurname = prompt("Inserisci il tuo cognome");
+const userSurname = prompt("Inserisci il tuo cognome");
 
 console.log(userSurname);
 
 
 // Chiedi il suo colore preferito
 
-userColour = prompt("Inserisci il tuo colore preferito");
+const userColour = prompt("Inserisci il tuo colore preferito");
 
 console.log(userColour);
 
 
 // Generazione Password = nomecognomecolorepreferito21
 
-userPassword = userName + userSurname + userColour + 21
+const userPassword = userName + userSurname + userColour + 21
 
 console.log(userPassword);
 
