f6199d4535642add07a4e8e5393af0be0d9f8637
CarloSalaris <carloa.salaris@gmail.com>
2023-04-26 14:58:09 +0200

    Generazione password + stampa risultato su pagina

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
 // Variables
+    let userName, userSurname, userColour, userPassword;
+    // const userName, userSurname, userColour;
 
-// const userName, userSurname, userColour;
 
 // Chiedi all’utente il suo nome
-const userName = prompt("Inserisci il tuo nome");
+userName = prompt("Inserisci il tuo nome");
 
 console.log(userName);
 
 
 // Chiedi il suo cognome
 
-const userSurname = prompt("Inserisci il tuo cognome");
+userSurname = prompt("Inserisci il tuo cognome");
 
 console.log(userSurname);
 
 
-
 // Chiedi il suo colore preferito
 
-const userColour = prompt("Inserisci il tuo colore preferito");
+userColour = prompt("Inserisci il tuo colore preferito");
 
 console.log(userColour);
 
 
+// Generazione Password = nomecognomecolorepreferito21
 
-// Scrivi sulla pagina nomecognomecolorepreferito21
+userPassword = userName + userSurname + userColour + 21
+
+console.log(userPassword);
+
+//Scrivi sulla pagina la nuova password generata
+
+document.getElementById("pwdGen").innerHTML = "Your new password is " + userPassword
