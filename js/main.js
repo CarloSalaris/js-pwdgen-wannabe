653fc0808a45d3bf2f072ac06e345d3ced589ed6
CarloSalaris <carloa.salaris@gmail.com>
2023-04-27 09:41:00 +0200

    Commented console.log()

--- js/main.js
+++ js/main.js
@@ -2,28 +2,28 @@
 // Chiedi all’utente il suo nome
 const userName = prompt("Inserisci il tuo nome");
 
-console.log(userName);
+// console.log(userName);
 
 
 // Chiedi il suo cognome
 
 const userSurname = prompt("Inserisci il tuo cognome");
 
-console.log(userSurname);
+// console.log(userSurname);
 
 
 // Chiedi il suo colore preferito
 
 const userColour = prompt("Inserisci il tuo colore preferito");
 
-console.log(userColour);
+// console.log(userColour);
 
 
 // Generazione Password = nomecognomecolorepreferito21
 
 const userPassword = userName + userSurname + userColour + 21
 
-console.log(userPassword);
+// console.log(userPassword);
 
 //Scrivi sulla pagina la nuova password generata
 
